7517d86b1d4ea26101bdfd26762204ac38ee890b
yhstsy <2798213571@qq.com>
2024-07-06 19:32:58 +0800

    create a test api route

---
+++ api/routes/user.route.js
@@ -0,0 +1,8 @@
+import express, { Route } from "express";
+import { test } from "../controller/user.controller.js";
+
+const router = express.Router();
+
+router.get('/test', test)
+
+export default router;
