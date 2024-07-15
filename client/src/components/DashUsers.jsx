0421d94bd74c62b5b5588e47ecca63d74f190bc7
yhstsy <2798213571@qq.com>
2024-07-15 22:36:44 +0800

    add delete user functionality to the admin user

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -48,7 +48,7 @@ export default function DashUsers() {
     setShowModal(false);
     try {
       const res = await fetch(
-        `/api/post/deleteuser/${userIdToDelete}/${currentUser._id}`,
+        `/api/user/delete/${userIdToDelete}`,
         {
           method: "DELETE",
         }
@@ -57,8 +57,8 @@ export default function DashUsers() {
       if (!res.ok) {
         console.log(data.message);
       } else {
-        setUserPosts((prev) =>
-          prev.filter((post) => post._id != postIdToDelete)
+        setUsers((prev) =>
+          prev.filter((user) => user._id != userIdToDelete)
         );
       }
     } catch (error) {
@@ -138,7 +138,7 @@ export default function DashUsers() {
           <div className="text-center">
             <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
             <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
-              Are you sure you want to delete this post?
+              Are you sure you want to delete this user?
             </h3>
             <div className="flex justify-center gap-4">
               <Button color="failure" onClick={handleDeleteUser}>
