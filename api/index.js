5ace96b144bb62a2e2fdd9a8cc230470e93bf5ba
yhstsy <2798213571@qq.com>
2024-07-21 11:46:48 +0800

    complete review fix

--- api/index.js
+++ api/index.js
@@ -35,9 +35,9 @@ app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/comment', commentRoutes)
 
-app.use(express.static(path.json(__dirname, '/client/dist')))
+app.use(express.static(path.join(__dirname, '/client/dist')))
 app.get('*', (req, res) => {
-    res.sendFile(path.json(__dirname, 'client', 'dist', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
 })
 
 app.use((err, req, res, next) => {
